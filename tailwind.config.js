d484fae64e1af5474f0b6e1fc3eae2d069c81d5e
Dragomir Yordanov <dyordanov@drvy.net>
2024-03-29 19:30:12 +0100

    Improve colour scheme and fix bugs

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,18 +1,30 @@
-module.exports = {
-    theme: {
-        extend: {
-            typography: {
-                DEFAULT: {
-                    css: {
-                        maxWidth: '100ch'
-                    }
+import colors from 'tailwindcss/colors'
+
+/** @type {import('tailwindcss').Config} */
+export const theme = {
+    colors: {
+        transparent: 'transparent',
+        current: 'currentColor',
+        black: colors.black,
+        white: colors.white,
+        gray: colors.gray,
+        dbg: '#f0f0f0',
+        dbb: 'rgb(210,210,210)',
+        dt: '#112a3d',
+        dtt: 'rgb(37,37,37)',
+        dbe: 'rgba(255,255,255,.8)',
+    },
+    extend: {
+        typography: {
+            DEFAULT: {
+                css: {
+                    maxWidth: '100ch'
                 }
             }
         }
-    },
-    darkMode: 'class',
-    content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.liquid'],
-    plugins: [
-        require('@tailwindcss/typography')
-    ],
+    }
 }
+export const content = ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.liquid']
+export const plugins = [
+    require('@tailwindcss/typography')
+]
