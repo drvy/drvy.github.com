06867f47809c7e2ef320eb8787b70f50899c2747
Dragomir Yordanov <dyordanov@drvy.net>
2024-03-29 14:15:16 +0100

    Increment max width for content

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,4 +1,15 @@
 module.exports = {
+    theme: {
+        extend: {
+            typography: {
+                DEFAULT: {
+                    css: {
+                        maxWidth: '100ch'
+                    }
+                }
+            }
+        }
+    },
     darkMode: 'class',
     content: ['./src/**/*.md', './src/**/*.html', './src/_includes/**/*.liquid'],
     plugins: [
